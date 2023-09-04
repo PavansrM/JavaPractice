cd9e6872748adf538883dc36d1143a1fc314bc0b
PavansrM <143777548+PavansrM@users.noreply.github.com>
2023-09-04 11:52:44 +0530

    initial commit of basic code
    Initial programs

---
+++ MyFirstProject/src/sum.java
@@ -0,0 +1,8 @@
+public class sum {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 3;
+        int sum = a + b;
+        System.out.println("This was the addtion of a and b " + sum);
+    } 
+}
